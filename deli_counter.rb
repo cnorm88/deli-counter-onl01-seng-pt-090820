354fec82e60017190843ca9c625719531ecc47fd
Crystal Norman <cnorm88@gmail.com>
2020-09-19 15:58:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,20 +12,6 @@ def line(names)
 end
 
 
-
-
-# def line(katz_deli)
-#   if katz_deli.length > 1
-#     tagline = "The line is currently:"
-#       katz_deli.each_with_index do |name, index|
-#       tagline << " #{index +1}. #{name}"
-#       end
-#     puts tagline
-#   else
-#     puts "The line is currently empty."
-#   end
-#   end
-
 # def take_a_number (katz_deli, string)
 #   if katz_deli.length > 1
 #     new_array = "Welcome, #{katz_deli}. You are number  in line."
