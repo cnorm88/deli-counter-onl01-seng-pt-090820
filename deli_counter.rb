b6f2af3865fac85179c28838f1ff1abaa1411fbc
Crystal Norman <cnorm88@gmail.com>
2020-09-19 16:20:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require 'pry'
+
+def line(names)
+  if names.empty?
+    puts "The line is currently empty."
+  else
+      positions = names.map.with_index(1) {|name, index| "#{index}. #{name}"}
+      string_positions = positions.join(" ")
+      puts "The line is currently: " + string_positions
+  end
+end
+
+def take_a_number(names, end_of_line)
+  names << end_of_line
+  puts "Welcome, #{end_of_line}. You are number #{names.length} in line."
+end
+
+def now_serving(names)
+  if names.empty?
+  puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{names.shift}."
+  end
+end
+ 
+
